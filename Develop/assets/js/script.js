5f1edc387de6d9ab9bd7de33bc60ba5494e54979
lundbmp <m.lundberg008@gmail.com>
2022-06-01 11:09:52 -0500

    moment is figured out, moving onto update calendar

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,14 @@
+/* 
+MomentJS
+need to establish moment to tell current local time
+    we will need an interval(n) assume N will be about 10000ms
+when we have local time printed to console, we can then  plan on how it fits in the code
+*/
+
+$(document).ready(updateCalendar());
+
+function updateCalendar() {
+    
+};
+
+//setInterval(updateCalendar, 10000);
