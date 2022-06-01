8bdca4c5f2d71a9e0708f112ac4a069e3f5594a5
lundbmp <m.lundberg008@gmail.com>
2022-06-01 11:36:28 -0500

    saving progress, can load the hour slot DOM into array

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,3 +1,5 @@
+let hourArray = [];
+
 /* 
 MomentJS
 need to establish moment to tell current local time
@@ -6,6 +8,28 @@ when we have local time printed to console, we can then  plan on how it fits in 
 */
 
 $(document).ready(updateCalendar());
+
+// hour slot object
+function hourSlotObj(id) {
+    this.elId = id;
+    this.hour = id.split("-")[1];
+}
+
+// function to load calendar from the DOM
+function loadCalendar() {
+    let targetDiv = $(".time-block").toArray();
+
+    for(let i = 0; i < targetDiv.length; i++) {
+        // placing DOM elements into hourArray as hourSlot obj
+        hourArray[i] = new hourSlotObj(targetDiv[i].id);
+    }
+}
+
+loadCalendar();
+
+hourArray.push(new hourSlot("hour-11"));
+
+console.log(hourArray[0].hour);
 
 function updateCalendar() {
     
